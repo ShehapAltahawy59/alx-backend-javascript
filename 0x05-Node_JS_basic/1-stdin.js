901b6c5863d4debe7c4dc42139e67c9658335f58
shehap62 <shehapkhalil62@gmail.com>
2024-09-18 10:52:31 +0300

    edit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,19 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (input) => {
-  const x = input.toString().trim();
-  console.log(`Your name is: ${x}`);
-  console.log('This important software is now closing');
-  process.exit();
-});
+
+if (process.stdin.isTTY) {
+    process.stdin.on('data', (input) => {
+        const x = input.toString().trim();
+        console.log(`Your name is: ${x}`);
+        process.exit();
+})}
+else {
+    process.stdin.on('data', (input) => {
+        const x = input.toString().trim();
+        console.log(`Your name is: ${x}`);
+        console.log('This important software is now closing');
+        process.exit();
+})}
+
+
+
+
