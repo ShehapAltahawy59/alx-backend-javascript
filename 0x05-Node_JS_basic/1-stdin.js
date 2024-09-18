6138932f33a66a0f66d328e3fa1b83acdecba8f0
shehap62 <shehapkhalil62@gmail.com>
2024-09-18 10:58:23 +0300

    edit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,18 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 if (process.stdin.isTTY) {
-    process.stdin.on('data', (input) => {
+  process.stdin.on('data', (input) => {
     const x = input.toString().trim();
     console.log(`Your name is: ${x}`);
     process.exit();
-    }); }
-else {
-    process.stdin.on('data', (input) => {
+  }); 
+}
+else
+{
+  process.stdin.on('data', (input) => {
     const x = input.toString().trim();
     console.log(`Your name is: ${x}`);
     console.log('This important software is now closing');
     process.exit();
-  });}
+  });
+}
